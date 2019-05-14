b1f28d9d7dc76708d231ac445f810cc994cfb637
Maikko Aleman <maikko.aleman@gmail.com>
2019-05-14 21:58:50 +0800

    Updates

---
+++ precache-manifest.8db83481fdad4d75467624ab29ed0e71.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7472e216e1e3fa055c8e1c3af46e271",
+    "url": "/medi/index.html"
+  },
+  {
+    "revision": "7c3bcb65403672b210c6",
+    "url": "/medi/static/css/main.d8fbd83f.chunk.css"
+  },
+  {
+    "revision": "e7c2f2f70349383dc89b",
+    "url": "/medi/static/js/2.0c4968c9.chunk.js"
+  },
+  {
+    "revision": "7c3bcb65403672b210c6",
+    "url": "/medi/static/js/main.037c381e.chunk.js"
+  },
+  {
+    "revision": "5059611a447de95241e1",
+    "url": "/medi/static/js/runtime~main.a47c026c.js"
+  },
+  {
+    "revision": "2989e9b0dff9359925598b8cead492c6",
+    "url": "/medi/static/media/graph.2989e9b0.svg"
+  },
+  {
+    "revision": "5f6ac0e6c50fd0c064bfded21da08f49",
+    "url": "/medi/static/media/logo.5f6ac0e6.svg"
+  }
+]);
